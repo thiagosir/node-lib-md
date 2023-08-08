0562e264d7307851822a78e178b05845674bd4cd
thiagosir <thiagobento9@hotmail.com>
2023-08-08 15:17:36 -0300

    Adicionando algumas libs

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('olá mundo');
+import chalk from 'chalk'
+import fs from 'fs';
+
+
+function catchFile (wayFile){
+    const encoding = 'utf-8';
+    fs.readFile(wayFile, encoding, (_, texto) => {
+        console.log(chalk.magenta.bold(texto));
+    })
+}
+ catchFile('./arquivos/texto.md')
