1d7b0f2278fcae88746a13b6eb183a56bac164b6
thiagosir <thiagobento9@hotmail.com>
2023-08-15 12:44:09 -0300

    Processando diretorios e tratando erros

--- src/cli.js
+++ src/cli.js
@@ -1,13 +1,39 @@
 import catchFile from './index.js'
+import fs from 'fs';
 import chalk from 'chalk';
 
 const caminho = process.argv;
 
-catchFile(caminho[2]);
+function printList(resultado, identificador = '' ) {
+    console.log(
+        chalk.yellow('Lista de Links -> '), 
+        chalk.black.bgGreenBright(identificador),
+        resultado);
+}
 
-async function processaTexto(caminho) {
-    const resultado = await catchFile(caminho[2]);
-    console.log(chalk.yellow('Lista de Links -> '), resultado);
+async function processaTexto(argumentos) {
+    const caminho = argumentos[2];
+
+    try {
+        fs.lstatSync(caminho)
+    } catch (erro) {
+        if (erro.code === 'ENOENT'){
+            console.log(chalk.red("Arquivo ou diretório não existente ;( "));
+            return;
+        }
+    }
+
+    if (fs.lstatSync(caminho).isFile()) {
+        const resultado = await catchFile(argumentos[2]);
+        printList(resultado);
+
+    } else if (fs.lstatSync(caminho).isDirectory()){
+        const arquivos = await fs.promises.readdir(caminho);
+        arquivos.forEach(async (fileName) => {
+            const lista = await catchFile(`${caminho}/${fileName}`);
+            printList(lista, fileName);
+        })
+    }
 }
 
 processaTexto(caminho);
